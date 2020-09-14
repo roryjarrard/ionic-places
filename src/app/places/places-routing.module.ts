7dd6caddb0121b912d9c87d3778d164c123512ff
Rory Jarrard <roryjarrard@gmail.com>
2020-09-14 16:21:43 -0600

    add tabs and default routing

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -1,20 +1,59 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { PlacesPage } from './places.page';
+import {PlacesPage} from './places.page';
+import {PlaceDetailPageModule} from './discover/place-detail/place-detail.module';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./discover/place-detail/place-detail.module').then(m => PlaceDetailPageModule)
+          }
+        ]
+      },
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./offers/offers.module').then(m => m.OffersPageModule)
+          },
+          {
+            path: 'new',
+            loadChildren: () => import('./offers/new-offer/new-offer.module').then(m => m.NewOfferPageModule)
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () => import('./offers/edit-offer/edit-offer.module').then(m => m.EditOfferPageModule)
+          },
+          {
+            path: ':placeId',
+            loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then(m => m.OfferBookingsPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
     path: '',
-    component: PlacesPage
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full'
   }
 ];
 
@@ -22,4 +61,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class PlacesPageRoutingModule {}
+export class PlacesPageRoutingModule {
+}
